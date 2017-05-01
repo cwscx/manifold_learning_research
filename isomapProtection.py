3fedf98627da28199688aa3bd3085a905590cfcf
Shengyang Shi <Shengyang Shi>
2017-05-01 10:14:10 -0700

    local commision

---
+++ isomapProtection.py
@@ -0,0 +1,45 @@
+import pickle
+import tensorflow as tf
+
+from tensorflow.examples.tutorials.mnist import input_data
+from sklearn import manifold
+
+from manifold_learn import *
+
+FILENAME = "isomap_model"
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+
+def helper(X):
+	return im.transform(X)
+
+if __name__ == "__main__":
+	mfo = manifold_learn(784, 100, 1, 100, 1000)
+	
+	im = mfo.readIsomap(FILENAME)
+		
+	x = tf.placeholder(tf.float32, [None, mfo.getInputDimension()])
+	reduced_x = tf.py_func(helper, [x], [tf.float64])[0]
+	reduced_x = tf.cast(reduced_x, tf.float32)
+	W = tf.Variable(tf.zeros([mfo.getIsomapDimension(), 10]))
+	b = tf.Variable(tf.zeros([10]))
+	y = tf.nn.softmax(tf.matmul(reduced_x, W) + b)
+
+	y_ = tf.placeholder(tf.float32, [None, 10])
+
+	cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
+
+	train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+
+	sess = tf.InteractiveSession()
+	tf.global_variables_initializer().run()
+
+	all_xs, all_ys = mfo.getXY()
+	for i in range(mfo.getIterTime()):
+		batch_xs = all_xs[i * mfo.getBatchSize() : (i + 1) * mfo.getBatchSize()]
+		batch_ys = all_ys[i * mfo.getBatchSize() : (i + 1) * mfo.getBatchSize()]
+		sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+
+	correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+
+	accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+	print(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
---
+++ manifold_learn.py
@@ -0,0 +1,65 @@
+import pickle
+import os
+import tensorflow as tf
+
+from tensorflow.examples.tutorials.mnist import input_data
+from sklearn import manifold
+
+class manifold_learn():
+
+	def __init__(self, input_dimension, isomap_dimension, knn, batch_size, iter_time):
+		if isomap_dimension > batch_size:
+			print("Error: BATCH_SIZE should not be less than ISOMAP_DIMENSION. \
+				Becuase the reducted dimension is min(ISOMAP_DIMENSINO, BATCH_SIZE)")
+			exit(1)
+
+		self.input_dimension = input_dimension
+		self.isomap_dimension = isomap_dimension
+		self.knn = knn
+		self.batch_size = batch_size 
+		self.iter_time = iter_time 
+
+	def getInputDimension(self):
+		return self.input_dimension
+
+	def getIsomapDimension(self):
+		return self.isomap_dimension
+
+	def getKNN(self):
+		return self.knn
+
+	def getBatchSize(self):
+		return self.batch_size
+
+	def getIterTime(self):
+		return self.iter_time
+
+	def saveXY(self, filename1="xs", filename2="ys"):
+		mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+		all_xs, all_ys = mnist.train.next_batch(self.iter_time * self.batch_size)
+		
+		pickle.dump(all_xs, open(filename1, "wb"))
+		pickle.dump(all_ys, open(filename2, "wb"))
+
+		return all_xs, all_ys
+
+	def getXY(self, filename1="xs", filename2="ys"):
+		if os.path.isfile(filename1) and os.path.isfile(filename2):
+			return pickle.load(open(filename1, "rb")), pickle.load(open(filename2, "rb"))
+		else:
+			return self.saveXY(filename1, filename2)
+
+	def trainAndSaveIsomap(self, filename="isomap_model"):
+		all_xs, all_ys = self.getXY("xs", "ys")
+
+		im = manifold.Isomap(self.knn, self.isomap_dimension)
+		im = im.fit(all_xs)
+		pickle.dump(im, open(filename, "wb"))
+
+		return im
+
+	def readIsomap(self, filename="isomap_model"):
+		if os.path.isfile(filename):
+			return pickle.load(open(filename, "rb"))
+		else:
+			return self.trainAndSaveIsomap(filename)
