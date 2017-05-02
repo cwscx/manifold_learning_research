incorrect value check

isomapProtection.py
@@ -13,7 +13,7 @@ def helper(X):
 	return im.transform(X)
 
 if __name__ == "__main__":
-	mfo = manifold_learn(784, 20, 1, 100, 400)
+	mfo = manifold_learn(784, 120, 3, 100, 100)
 	im = mfo.readIsomap(FILENAME)
 		
 	x = tf.placeholder(tf.float32, [None, mfo.getInputDimension()])