39f12692155533431be1134720de099f4a7426e7
Shengyang Shi <shengyang.shi@hotmail.com>
2017-07-26 15:25:59 -0700

    read Cifar

--- readCifar.py
+++ readCifar.py
@@ -1,18 +1,48 @@
-filename = "./cifar/test_batch"
+import tarfile, os, pickle
 
-def unpickle(file):
-    import pickle
-    with open(file, 'rb') as fo:
-        dict = pickle.load(fo, encoding='bytes')
-    return dict
+filename = "./Cifar data/cifar-10-python.tar.gz"
+baseDir = "./Cifar data/"
+dataDir = baseDir + "cifar-10-batches-py/"
+train_batch = dataDir + "data_batch_"
+test_batch = dataDir + "test_batch"
 
+file = tarfile.open(filename)
 
-def getByteKey(key):
+# Extract if not extracted
+if not os.path.exists(dataDir):
+	file.extractall(baseDir)
+
+file.close()
+
+def getAllTrainBatchData():
+	data = list()
+	for i in range(1, 6):
+		data.append(getTrainBatchData(i))
+
+	return data
+
+def getTrainBatchData(batch_id):
+	# Check validity
+	if batch_id > 5 or batch_id < 1 or (batch_id != int(batch_id)):
+		return dict()
+
+	train_filename = train_batch + str(batch_id)
+	with open(train_filename, "rb") as fo:
+		data = pickle.load(fo, encoding="bytes")
+
+	return data
+
+def getTestBatchData():
+	with open(test_batch, "rb") as fo:
+		data = pickle.load(fo, encoding="bytes")
+
+	return data
+
+def toByteKey(key):
 	return bytes(key, "utf-8")
 
-"""
-print(unpickle(filename).keys())
-a = unpickle(filename)[getByteKey("data")][0]
-"""
+def getLabels(data_dict):
+	return data_dict.get(toByteKey("labels"))
 
-print(len(unpickle("./cifar/test_batch")[getByteKey("data")]))
+def getData(data_dict):
+	return data_dict.get(toByteKey("data"))
