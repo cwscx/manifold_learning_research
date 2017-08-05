345fd2bac13cfece908439a3f8affaf9cfcfa348
Shengyang Shi <shengyang.shi@hotmail.com>
2017-08-05 00:01:13 -0700

    over 60%

--- cifarCNN.py
+++ cifarCNN.py
@@ -7,52 +7,64 @@ from readCifar import *
 cifar_train_batch_1 = getTrainBatchData(1)
 cifar_test_batch = getTestBatchData()
 
-"""
-def weight_variable(shape):
-	initial = tf.truncated_normal(shape, stddev=0.1)
+test_image = getData(cifar_test_batch)
+test_label = getLabels(cifar_test_batch)
+
+def weight_variable(shape, stddev):
+	initial = tf.truncated_normal(shape, stddev=stddev)
 	return tf.Variable(initial)
 
-def bias_variable(shape):
-	initial = tf.constant(0.1, shape=shape)
+def bias_variable(shape, constant):
+	initial = tf.constant(constant, shape=shape)
 	return tf.Variable(initial)
 
 def conv2d(x, W):
 	return tf.nn.conv2d(x, W, strides=[1,1,1,1], padding="SAME")
 
 def max_pool_2x2(x):
-	return tf.nn.max_pool(x, ksize=[1,2,2,1], strides=[1,2,2,1], padding="SAME")
+	return tf.nn.max_pool(x, ksize=[1,3,3,1], strides=[1,2,2,1], padding="SAME")
 
 if __name__ == "__main__":
 
-	W_conv1 = weight_variable([5,5,1,32])
-	b_conv1 = bias_variable([32])
+	W_conv1 = weight_variable([5,5,3,64], 5e-2)
+	b_conv1 = bias_variable([64], 0.0)
 
-	x = tf.placeholder(tf.float32, shape=[None, 784])
-	x_image = tf.reshape(x, [-1,28,28,1])
+	x = tf.placeholder(tf.float32, shape=[100, 3072])
+	x_image = tf.reshape(x, [-1,32,32,3])
 
 	h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
 	h_pool1 = max_pool_2x2(h_conv1)
+	h_norm1 = tf.nn.lrn(h_pool1, 4, bias=1.0, alpha=0.001 / 9.0, beta=0.75,
+                    name='norm1')
 
-	W_conv2 = weight_variable([5,5,32,64])
-	b_conv2 = bias_variable([64])
+	W_conv2 = weight_variable([5,5,64,64], 5e-2)
+	b_conv2 = bias_variable([64], 0.1)
 
-	h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
-	h_pool2 = max_pool_2x2(h_conv2)
+	h_conv2 = tf.nn.relu(conv2d(h_norm1, W_conv2) + b_conv2)
+	h_norm2 = tf.nn.lrn(h_conv2, 4, bias=1.0, alpha=0.001 / 9.0, beta=0.75,
+                    name='norm2')
+	h_pool2 = max_pool_2x2(h_norm2)
+	reshape = tf.reshape(h_pool2, [100, -1])
 
-	W_fc1 = weight_variable([7 * 7 * 64, 1024])
-	b_fc1 = bias_variable([1024])
+	weight_dim = reshape.get_shape()[1].value
 
-	h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 64])
-	h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+	W_fc1 = weight_variable([weight_dim, 384], 4e-2)
+	b_fc1 = bias_variable([384], 0.1)
+	
+	h_fc1 = tf.nn.relu(tf.matmul(reshape, W_fc1) + b_fc1)
 
-	keep_prob = tf.placeholder(tf.float32)
-	h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+	W_fc2 = weight_variable([384, 192], 4e-2)
+	b_fc2 = bias_variable([192], 0.1)
 
-	W_fc2 = weight_variable([1024, 10])
-	b_fc2 = bias_variable([10])
+	h_fc2 = tf.nn.relu(tf.matmul(h_fc1, W_fc2) + b_fc2)
 
-	y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
+	W_fc3 = weight_variable([192, 10], 1/192)
+	b_fc3 = bias_variable([10], 0.0)
+
+	y_conv = tf.matmul(h_fc2, W_fc3) + b_fc3
 	y_ = tf.placeholder(tf.float32, [None, 10])
+
+	print(y_conv.shape)
 
 	learning_rate = 1e-4
 
@@ -64,10 +76,39 @@ if __name__ == "__main__":
 
 	correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
 	accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-
+	
 	sess = tf.InteractiveSession()
 	sess.run(tf.global_variables_initializer())
-	
+
+	for j in range(1, 201):
+		cifar_train_batch = getTrainBatchData(j)
+		
+		if (j / 5) % 2 == 0:	
+			train_images = getData(cifar_train_batch)
+		else:
+			train_images = getFlipData(cifar_train_batch)
+
+		train_labels = getLabels(cifar_train_batch)
+
+		for i in range(100):
+			batch_images = train_images[i * 100 : (i + 1) * 100]
+			batch_labels = train_labels[i * 100 : (i + 1) * 100]
+
+			train_accuracy = accuracy.eval(feed_dict={x:batch_images, y_: batch_labels})
+			print("step %d, training accuracy %g" %((j - 1) * 100 + i, train_accuracy))
+
+			train_step.run(feed_dict={x: batch_images, y_: batch_labels})
+
+		if j % 10 == 0:
+			acc = 0.0
+
+			for i in range(100):
+				acc += accuracy.eval(feed_dict={x: test_image[i * 100 : (i + 1) * 100], y_: test_label[i * 100 : (i + 1) * 100]})
+
+			acc /= 100.0
+			print("test accuracy %g"% acc)
+
+	"""
 	saver = tf.train.Saver()
 	model_dir = "./tmp/cnnModel.ckpt"
 
@@ -75,22 +116,20 @@ if __name__ == "__main__":
 		saver.restore(sess, model_dir)
 		print("Model restored")
 	else:
+		
 		for i in range(20000):
 			batch = mnist.train.next_batch(50)
 			if i%100 == 0:
 				train_accuracy = accuracy.eval(feed_dict={x:batch[0], y_: batch[1], keep_prob: 1.0})
 				print("step %d, training accuracy %g"%(i, train_accuracy))
 			train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
-
+	
 		save_path = saver.save(sess, "./tmp/cnnModel.ckpt")
 		print("Model saved in file: %s" % save_path)
+	"""
 
 	# print("test accuracy %g"%accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
 
-	test_image = [mnist.test.images[0]]
-	test_label = [mnist.test.labels[0]]
 
-	print("test accuracy %g"%accuracy.eval(feed_dict={x: test_image, y_: test_label, keep_prob: 1.0}))
-	
 	sess.close()
-	"""
+	
--- readCifar.py
+++ readCifar.py
@@ -1,4 +1,8 @@
 import tarfile, os, pickle
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.misc import toimage
+
 
 filename = "./Cifar data/cifar-10-python.tar.gz"
 baseDir = "./Cifar data/"
@@ -23,8 +27,12 @@ def getAllTrainBatchData():
 
 def getTrainBatchData(batch_id):
 	# Check validity
-	if batch_id > 5 or batch_id < 1 or (batch_id != int(batch_id)):
+	if batch_id < 1 or (batch_id != int(batch_id)):
 		return dict()
+
+	batch_id = batch_id % 5
+	if batch_id == 0:
+		batch_id = 5
 
 	train_filename = train_batch + str(batch_id)
 	with open(train_filename, "rb") as fo:
@@ -41,8 +49,35 @@ def getTestBatchData():
 def toByteKey(key):
 	return bytes(key, "utf-8")
 
+def toListLabel(index):
+	ret = [0.0] * 10
+	ret[index] = 1.0
+	return ret
+
 def getLabels(data_dict):
-	return data_dict.get(toByteKey("labels"))
+	return np.array([np.array(toListLabel(label)) for label in data_dict.get(toByteKey("labels"))])
+
+def transformData(data, mean=0.0, stddev=1.0):
+	means = [sum(d) / len(d) for d in data]
+	stddevs = [(sum((d - (sum(d) / len(d))) ** 2) / (len(d) - 1)) ** 0.5 for d in data]
+	return [(data[i] + mean - means[i]) * stddev / stddevs[i] for i in range(len(data))]
 
 def getData(data_dict):
-	return data_dict.get(toByteKey("data"))
+	ret = data_dict.get(toByteKey("data"))
+	return transformData(ret)
+
+def getFlipData(data_dict):
+	return list(map(lambda x: flipImg(x), getData(data_dict)))
+
+def flipImg(img):
+	new_img = [0] * len(img)
+	for i in range(int(len(img) / 32)):
+		for j in range(32):
+			new_img[i * 32 + j] = img[i * 32 + (31 - j)]
+
+	return new_img
+
+def displayImg(img):
+	tmpimg = np.array(img).reshape(3,32,32).transpose(1,2,0)
+	plt.imshow(tmpimg)
+	plt.show()
